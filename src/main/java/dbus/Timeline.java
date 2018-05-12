cc67a1eb2ace691dddb5f5d66f34311b32154830
Dorian <bussidn@gmail.com>
2018-05-12 18:29:43 +0200

    add Timeline documentation

--- src/main/java/dbus/Timeline.java
+++ src/main/java/dbus/Timeline.java
@@ -2,5 +2,11 @@ package dbus;
 
 import java.util.Date;
 
+/**
+ * This abstraction represents a timeline in the sense of it is
+ * a container containing ordered date instances
+ *
+ * @param <E> a java.util.Date or an extension of it
+ */
 public interface Timeline<E extends Date> {
 }
